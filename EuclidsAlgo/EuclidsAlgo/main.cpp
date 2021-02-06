7a7ae531a6a2ee42037e26fda3089b6c0c9f7d4c
Martin Townley <mtown001@gold.ac.uk>
2021-02-06 19:01:05 +0000

    Fraction struct uses Euclid's algorithm function to return fraction in its lowest terms

--- EuclidsAlgo/EuclidsAlgo/main.cpp
+++ EuclidsAlgo/EuclidsAlgo/main.cpp
@@ -8,28 +8,52 @@
 
 #include <iostream>
 
-int gcd(int u, int v) {
-    int t;
-    while (u > 0)
+struct Fraction {
+    
+public:
+    Fraction()
     {
-        if (u < v) //switch their places
-        {
-            t = u; //store u in t
-            u = v; //set u to v
-            v = t; //set v to t (previously u)
-        }
-        u = u % v;
+        int x, y;
+        while (std::cin >> x && std::cin >> y)
+            if (x>0 && y>0)
+            {
+                calculateLowestTerms(x, y);
+                
+                std::cout << numerator << '/' << denominator << std::endl;
+            }
     }
-    return v;
-}
+        
+    int gcd(int u, int v) {
+        int t;
+        while (u > 0)
+        {
+            if (u < v) //switch their places
+            {
+                t = u; //store u in t
+                u = v; //set u to v
+                v = t; //set v to t (previously u)
+            }
+            u = u % v;
+        }
+        return v;
+    }
+    
+    void calculateLowestTerms(int u, int v) {
+        numerator = u / gcd(u, v);
+        denominator = v / gcd(u, v);
+    }
+    
+    int getNumerator() { return numerator; }
+    int getDenominator() { return denominator; }
+    
+private:
+    
+    int numerator;
+    int denominator;
+};
 
 int main(int argc, const char * argv[]) {
     
-    int x, y;
-    while (std::cin >> x && std::cin >> y)
-        if (x>0 && y>0) std::cout << x << ' ' << y << ' '
-            << gcd(x,y) << '\n';
-    
-    
+    git Fraction myFrac;
     //return 0;
 }
